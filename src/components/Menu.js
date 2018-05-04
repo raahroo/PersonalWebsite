5a7763e69a871ce3182d5e89b6fcd15fe7eee302
Raissa Hohenester <raissa.developer@gmail.com>
2018-05-04 12:30:07 +0200

    Read Me

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+    render() {
+        return (
+            <h1 className="Menu">Menu</h1>
+        );
+    }
+}
+
+export default Menu;
