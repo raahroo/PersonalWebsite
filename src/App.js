0af984a717dbd4457cc255b4ce5ff41ae51a0a8f
Raissa Hohenester <raissa.developer@gmail.com>
2018-05-09 17:28:12 +0200

    einige Stile, Svg und Befestigung

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <MenuContainer />
         <Main />
+        <MenuContainer />
+        
       </div>
     );
   }
